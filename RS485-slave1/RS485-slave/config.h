1807bbd27c8fd9f0f0864d94ae75ec02612cf9a6
binhnt1612 <binhnt1612@gmail.com>
2019-06-26 21:27:55 +0700

    Update config.h

--- RS485-slave1/RS485-slave/config.h
+++ RS485-slave1/RS485-slave/config.h
@@ -10,28 +10,28 @@
 #define __CONFIG_H
 
 #ifndef F_CPU
-	#define F_CPU 16000000UL
+    #define F_CPU 16000000UL
 #endif
 
 #define RS485_SYNCBYTE		0x17
 #define NUM_OF_PACKET		2
 
-#define RS485_DE			6
-#define RS485_RE			7
+#define RS485_DE		6
+#define RS485_RE		7
 
-#define ENABLE_TX			PORTD |= (1 << RS485_DE)
-#define DISABLE_TX			PORTD &= ~(1 << RS485_DE)
+#define ENABLE_TX		PORTD |= (1 << RS485_DE)
+#define DISABLE_TX		PORTD &= ~(1 << RS485_DE)
 
-#define ENABLE_RX			PORTD &= ~(1 << RS485_RE)
-#define DISABLE_RX			PORTD |= (1 << RS485_RE)
+#define ENABLE_RX		PORTD &= ~(1 << RS485_RE)
+#define DISABLE_RX		PORTD |= (1 << RS485_RE)
 
-#define SLAVE1_ADDR			0x02
+#define SLAVE1_ADDR		0x02
 
-#define GET_TEMPERATURE     0x02
-#define GET_LEDSTATUS       0x03
+#define GET_TEMPERATURE         0x02
+#define GET_LEDSTATUS           0x03
 
 //NTC Temparature config
-#define CONST_RES			9800
+#define CONST_RES		9800
 #define OFFSET_CALIB		1.15  
 
 #endif
