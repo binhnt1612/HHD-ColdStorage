0183f1d98a0a00654e4c8c538c0356555ff03da1
binhnt1612 <binhnt1612@gmail.com>
2019-06-26 21:09:49 +0700

    Update config.h

--- RS485-master/RealMaster/config.h
+++ RS485-master/RealMaster/config.h
@@ -10,39 +10,37 @@
 #define __CONFIG_H
 
 #ifndef F_CPU
-	#define F_CPU 16000000UL
+    #define F_CPU 16000000UL
 #endif
 
 #define HOST_SYNCBYTE       0x16
-#define RS485_SYNCBYTE		0x17
-#define NUM_OF_PACKET		10
+#define RS485_SYNCBYTE	    0x17
+#define NUM_OF_PACKET	    10
 
-#define RS485_DE			6
-#define RS485_RE			7
-#define BUZZER_PIN			1
-#define BUZZER_TIMEOUT		15
+#define RS485_DE	    6
+#define RS485_RE	    7
+#define BUZZER_PIN	    1
+#define BUZZER_TIMEOUT	    15
 
-#define ENABLE_TX			PORTD |= (1 << RS485_DE)
-#define DISABLE_TX			PORTD &= ~(1 << RS485_DE)
+#define ENABLE_TX	    PORTD |= (1 << RS485_DE)
+#define DISABLE_TX	    PORTD &= ~(1 << RS485_DE)
 
-#define ENABLE_RX			PORTD &= ~(1 << RS485_RE)
-#define DISABLE_RX			PORTD |= (1 << RS485_RE)
+#define ENABLE_RX	    PORTD &= ~(1 << RS485_RE)
+#define DISABLE_RX	    PORTD |= (1 << RS485_RE)
 
-#define MASTER_ADDR			0x01
-#define SLAVE1_ADDR			0x02
-#define SLAVE2_ADDR			0x03
-#define SLAVE3_ADDR			0x04
-#define SLAVE4_ADDR			0x05
-#define SLAVE5_ADDR			0x06
-
-
-#define SETTIME_INTERVAL    0x01	
+#define MASTER_ADDR	    0x01
+#define SLAVE1_ADDR	    0x02
+#define SLAVE2_ADDR	    0x03
+#define SLAVE3_ADDR	    0x04
+#define SLAVE4_ADDR         0x05
+#define SLAVE5_ADDR	    0x06
+	
 #define GET_TEMPERATURE     0x02
 #define GET_LEDSTATUS       0x03
 
-#define MAX_BUZZER			1
-#define BUZZER_ADDR			0x07
-#define BUZZER_ON			0x01
-#define BUZZER_OFF			0x02
+#define MAX_BUZZER	    1
+#define BUZZER_ADDR	    0x07
+#define BUZZER_ON	    0x01
+#define BUZZER_OFF	    0x02
 
 #endif
